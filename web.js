1dcf60f514066fc7e638531062ee60d8fd362eaa
Tim Jaeger <tim.jaeger@gmail.com>
2013-07-08 00:58:18 +0000

    tried to fix error again

--- web.js
+++ web.js
@@ -14,7 +14,7 @@ fs.readFile('index.html', function (err, data) {
 });
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
