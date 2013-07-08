dedb93af86f23dcecb1132fb89d04f9202d2f860
Tim Jaeger <tim.jaeger@gmail.com>
2013-07-08 01:26:28 +0000

    fixed error

--- web.js
+++ web.js
@@ -2,10 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer();
+var buffer = new Buffer(1024);
 
 fs = require('fs');
-
 
 fs.readFile('index.html', function (err, data) {
   if (err) throw err;
